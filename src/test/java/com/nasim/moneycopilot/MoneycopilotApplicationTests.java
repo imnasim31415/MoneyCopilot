c5cf8c1e0689a125dc4df646260a609af5d941b6
Nasim Hossain Rabbi <56127958+imnasim31415@users.noreply.github.com>
2026-04-11 02:41:06 +0600

    Add project setup documentation and configuration files (#5)
    * chore: add docs and repo config files
    
    * chore: create project setup branch with initial repo files and checkstyle configuration

---
+++ src/test/java/com/nasim/moneycopilot/MoneycopilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nasim.moneycopilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneycopilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
